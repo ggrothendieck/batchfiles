d8d69b2b25bf44518fd9b0a8c9fad27580d20873
ggrothendieck <ggrothendieck@cad02e4e-d21f-0410-b61e-c98ab725bed9>
2007-01-26 03:04:05 +0000

    R extensions manual attribution for Rtidy

--- trunk/Rtidy.bat
+++ trunk/Rtidy.bat
@@ -1,5 +1,6 @@
 #Rscript %0.bat %*
 # usage: Rtidy sourcefile.R > sourcefile.Rtidy.R
+# based on 3.1 of R Extensions Manual
 options(keep.source = FALSE)
 source(commandArgs(TRUE))
 dump(ls(all = TRUE), file = stdout())
