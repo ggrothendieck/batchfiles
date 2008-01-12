d9b9e357057e8c48543f8ef67bf0783c5b66e8b3
ggrothendieck <ggrothendieck@cad02e4e-d21f-0410-b61e-c98ab725bed9>
2008-01-12 18:44:32 +0000

    eliminated dependency of sweave.bat on Rterm.bat

--- trunk/sweave.bat
+++ trunk/sweave.bat
@@ -6,6 +6,10 @@ if "%1"=="" goto:help
 if "%1"=="-h" goto:help
 if "%1"=="--help" goto:help
 if "%1"=="/?" goto:help
+if "%1"==":Rterm" (
+	shift
+	goto:Rterm
+)
 goto:continue
 :help
 echo Usage: sweave abc.Rnw
@@ -57,9 +61,9 @@ if exist "%file%.Rtex" set infile="%file%.Rtex"
 if exist "%file%.Snw" set infile="%file%.Snw"
 if exist "%file%.Rnw" set infile="%file%.Rnw"
 if exist "%file%" set infile="%file%" 
-set infileslash=%infile:\=/%
+set infilslsh=%infile:\=/%
 :: call sweave
-echo library('utils'); Sweave(%infileslash%) | Rterm --no-restore --slave
+echo library('utils'); Sweave(%infilslsh%) | sweave.bat :Rterm --no-restore --slave
 :: echo on
 if errorlevel 1 goto:eof
 if /i "%switch%"=="t" goto:eof
@@ -87,6 +91,117 @@ set tmpfile=%base%-%tmpfile%.bck.pdf
 copy "%pdffile%" "%tmpfile%"
 start "" "%tmpfile%"
 echo *** delete *.bck.pdf files when done ***
+goto:eof
+
+
+
+@echo off
+setlocal
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Placing this file in your path will allow rcmd to be run anywhere
+:: without changing your path environment variable.  See comments
+:: below on how it finds where R is.  Your path can be listed by
+:: the Windows console command:  path
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: use environment variable R_HOME if defined
+:: else current folder if bin\rcmd.exe exists 
+:: else most current R as determined by registry entry
+:: else error
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+if not defined R_HOME if exist bin\rcmd.exe set R_HOME=%CD%
+if not defined R_HOME for /f "tokens=2*" %%a in (
+ 'reg query hklm\software\r-core\r /v InstallPath 2^>NUL ^| findstr InstallPath'
+  ) do set R_HOME=%%~b
+if not defined R_HOME echo "Error: R not found" & goto:eof
+
+set here=%CD%
+set args=%*
+
+:: get name by which this command was called
+:: this allows same file to be used for Rgui, Rterm, etc. by just renaming it
+for %%i in (%0) do set cmd=%%~ni.exe
+
+goto %cmd%
+goto:eof
+
+:: note that RguiStart sets cmd to rgui.exe and then 
+:: jumps to :Rgui.exe where processing is finished
+:RguiStart
+:RguiStart.exe
+cd %R_HOME%\bin
+if /i not %cmd%==rguistart.exe goto:Rgui
+  set cmd=rgui.exe
+  set firstArgument=%1
+  if defined firstArgument (
+    dir %1 | findstr "<DIR>" > nul
+    if errorlevel 1 goto:notRguiStart
+    set here=%~1
+    set firstArgument=
+  )
+  set args=
+  shift
+  :startloop
+  set firstArgument=%1
+  if defined firstArgument (
+     set args=%args% "%~1" 
+     shift
+     goto:startloop
+  )
+
+:Rcmd2
+:Rcmd2.exe
+:Rcmd
+:Rcmd.exe
+(set cmd=rcmd.exe)&goto:main
+
+:Rterm
+:Rterm.exe
+(set cmd=rterm.exe)&goto:main
+
+:Rgui
+:Rgui.exe
+(set cmd=rgui.exe)&goto:main
+
+:R
+:R.exe
+(set cmd=r)&goto:main
+
+:Rjgr
+:Rjgr.exe
+(set cmd=rjgr)&goto:main
+
+:#Rscript
+:#Rscript.exe
+:Rscript
+:Rscript.exe
+(set cmd=rscript.exe)&goto:main
+
+# main portion of program
+:main
+set st=
+if /i %cmd%==rgui.exe set st=start
+:: if /i %cmd%==#rscript.exe set cmd=rscript.exe
+cd %here%
+set cmdpath=%R_HOME%\bin\%cmd%
+
+:: if called as jgr.bat locate the JGR package to find jgr.exe
+if /i %cmd%==jgr.exe (
+  set st=start
+  set cmdpath=jgr.exe
+  for /f "delims=" %%i in (JGR) do set jgrpkg=%%~$R_LIBS:i
+  if defined jgrpkg set cmdpath=%jgrpkg%\jgr.exe
+  if exist "%R_HOME%\library\JGR\jgr.exe" set cmdpath=%R_HOME%\library\JGR\jgr.exe
+) 
+
+echo if defined st (start "" "%cmdpath%" %args%) else "%cmdpath%" %args%
+if defined st (start "" "%cmdpath%" %args%) else "%cmdpath%" %args%
+goto:eof
 
 endlocal
+
+
+endlocal
+
 
