df6200f629c9fad329d4827334e0ae8a5722cd9d
ggrothendieck <ggrothendieck@cad02e4e-d21f-0410-b61e-c98ab725bed9>
2013-11-04 04:15:13 +0000

    another fix

--- trunk/clip2r.js
+++ trunk/clip2r.js
@@ -4,8 +4,8 @@
 // To use with vim place these two lines in your _vimrc file
 // To find the directory where _vimrc goes enter this in vim   :echo $HOME
 // 
-//    map <F3> <C-C><ESC>:!cmd /k "start clip2r.js"<CR><CR>
-//    imap <F3> <ESC><C-C><ESC>:!cmd /k "start clip2r.js"<CR><CR>
+//    map <F3> <C-C><ESC>:!start cmd /c clip2r.js<CR><CR>
+//    imap <F3> <ESC><C-C><ESC>:!start cmd /c clip2r.js<CR><CR>
 
 var wsh = new ActiveXObject("Wscript.Shell");
 wsh.AppActivate("Rgui");
