aa8a7970a0857811a18c13a89dcd01dafe74fff7
ggrothendieck <ggrothendieck@cad02e4e-d21f-0410-b61e-c98ab725bed9>
2013-11-04 04:04:41 +0000

    bug fix

--- clip2r.js
+++ clip2r.js
@@ -1,25 +1,11 @@
 // (c) 2009.  Gabor Grothendieck. 
 // This is free software licensed under GPL.
 //
-// Based on TpR.bas by Philippe Grosjean.
-//
-// 1. To find the directory where _vimrc goes enter this in vim   :echo $HOME
-// 2. Place these two lines in your _vimrc file in which case F3 will 
-//    transmit current selection to the clipboard and then paste it into R
-//    e.g. ctrl-A F3 copies entire file to R
-//
-//    map <F3> <C-C><ESC>:!start clip2r.js<CR><CR>
-//    imap <F3> <ESC><C-C><ESC>:!start clip2r.js<CR><CR>
-//
-//    Alternately use these two lines copy entire file when F3 is pressed:
+// To use with vim place these two lines in your _vimrc file
+// To find the directory where _vimrc goes enter this in vim   :echo $HOME
 // 
-//    map <F3> <C-A><C-C><ESC>:!start clip2r.js<CR><CR>
-//    imap <F3> <ESC><C-A><C-C><ESC>:!start clip2r.js<CR><CR>
-//
-// Note: In the wsh.SendKeys command the strings: 
-//       "% x" stands for Alt-space x and has the effect of maximizing Rgui
-//       "%{w}1" stands for Alt-w 1 and activates the R console
-// 		 "^{v}" stands for ctrl-v and pastes clipboard.
+//    map <F3> <C-C><ESC>:!cmd /k "start clip2r.js"<CR><CR>
+//    imap <F3> <ESC><C-C><ESC>:!cmd /k "start clip2r.js"<CR><CR>
 
 var wsh = new ActiveXObject("Wscript.Shell");
 wsh.AppActivate("Rgui");
